4bf271cbbeffa6272a66be5e543da3278698c02e
George Bobby <georgebobbyawesome@gmail.com>
2025-03-21 13:19:56 +0530

    automatically close menu after redirecting to next page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Menu, ChevronRight } from "lucide-react";
@@ -9,6 +9,11 @@ import { cn } from "@/lib/utils";
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const pathname = usePathname();
+
+  // Add effect to close menu when pathname changes
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [pathname]);
 
   const navLinks = [
     { href: "/home", label: "Home" },
